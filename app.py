bce8843c8df939bde0f4887c51ed07ae85b08b3b
Dhanushka <dhanushkamcr@gmail.com>
2025-08-25 18:01:35 -0500

    Creating Models

--- app.py
+++ app.py
@@ -6,11 +6,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
 from sqlalchemy import ForeignKey, Table, Column, String, Integer
 from marshmallow import ValidationError
 from typing import List, Optional
-from __future__ import annotations
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -28,3 +27,34 @@ db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 ma = Marshmallow(app)
 
+# Association Table
+user_pet = Table(
+    "user_pet",
+    Base.metadata,
+    Column("user_id", ForeignKey("user_account.id"), primary_key=True),
+    Column("pet_id", ForeignKey("pets.id"), primary_key=True)
+)
+
+# Models
+class User(Base):
+    __tablename__ = "user_account"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    email: Mapped[Optional[str]] = mapped_column(String(200))
+    
+    #One-to-Many relationship from this User to a List of Pet Objects
+    pets: Mapped[List["Pet"]] = relationship("Pet", secondary=user_pet, back_populates="owners")
+
+class Pet(Base):
+    __tablename__ = "pets"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+    animal: Mapped[str] = mapped_column(String(200), nullable=False)
+    
+    # One-to-Many relationship, One pet can be related to a List of Users
+    owners: Mapped[List["User"]] = relationship("User", secondary=user_pet, back_populates="pets")
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
