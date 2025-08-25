4f5e24d7fb2bc698566af3c69a451b724caf46e6
Dhanushka <dhanushkamcr@gmail.com>
2025-08-25 17:49:54 -0500

    Set Up the Flask App and MySQL Connection

--- app.py
+++ app.py
@@ -2,3 +2,29 @@
 # venv\Scripts\Activate
 # pip install Flask Flask-SQLAlchemy Flask-Marshmallow mysql-connector-python marshmallow-sqlalchemy
 # CREATE DATABASE flask_api_db;
+
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy import ForeignKey, Table, Column, String, Integer
+from marshmallow import ValidationError
+from typing import List, Optional
+from __future__ import annotations
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# MySQL database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:1234@localhost/flask_api_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Creating our Base Model
+class Base(DeclarativeBase):
+    pass
+
+# Initialize SQLAlchemy and Marshmallow
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+ma = Marshmallow(app)
+
